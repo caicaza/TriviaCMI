716e443c32e6cb162ade6f66bef79187e99b9ddd
Digtester454 <141531017+Digtester454@users.noreply.github.com>
2023-10-20 17:43:01 -0500

    correcion url

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 
-export const environment = { URL: 'https://www.triviacmi.somee.com/' };
+export const environment = { URL: 'https://www.triviacmi.somee.com' };
 //export const environment = { URL: 'http://192.168.100.91:8081' };
 //export const environment = { URL: 'http://192.168.1.9:8081' };
 //export const environment = { URL: 'http://172.20.10.7:8081' };
