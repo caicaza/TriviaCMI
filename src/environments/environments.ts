b0d8e2ffa59bb8755a430daebc986a4803855e0b
Digtester454 <141531017+Digtester454@users.noreply.github.com>
2023-10-20 10:16:48 -0500

    Segunda ola

---
+++ src/environments/environments.ts
@@ -0,0 +1,6 @@
+export const environment = { URL: 'http://192.168.100.91:8081' };
+//export const environment = { URL: 'http://192.168.1.9:8081' };
+//export const environment = { URL: 'http://172.20.10.7:8081' };
+//export const environment = { URL: 'http://localhost:38755' };
+
+export const encryptionKey = 'MiClaveSecreta23ParaLaTrivia_CMI';
